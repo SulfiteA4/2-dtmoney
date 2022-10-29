45e57739f2f3de8fba3130254394949a978d7a52
SulfiteA4 <lhungaro10@gmail.com>
2022-10-29 00:04:15 -0300

    add bib react-modal e add componente Modal

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,14 +1,34 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global"
 
+Modal.setAppElement('#root');
+
 export function App() {
-  return (
-	<>
-		<Header />
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal(){ //abre o modal
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal(){ // fecha o modal
+        setIsNewTransactionModalOpen(false);
+    }
+		
+	return (
+		<>
+			<Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 			<Dashboard />
-		<GlobalStyle />
-	</>
-  );
+			<Modal
+				isOpen={isNewTransactionModalOpen}
+				onRequestClose={handleCloseNewTransactionModal}
+			>
+				<h2>Cadastrar transação</h2>
+			</Modal>
+			<GlobalStyle />
+		</>
+	);
 }
 
