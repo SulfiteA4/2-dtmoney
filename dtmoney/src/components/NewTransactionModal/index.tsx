53071f589e3cd5c58bea22305f8544cddb814ae4
SulfiteA4 <lhungaro10@gmail.com>
2022-10-29 11:00:54 -0300

    add estilização do modal

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import { Container } from './style';
+import closeImg from '../../assets/close.svg';
 import Modal from 'react-modal';
 
 interface NewTransactionModalProps {
@@ -14,6 +15,13 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             overlayClassName="react-modal-overlay"
             className="react-modal-content"
         >
+            <button 
+                type="button" 
+                onClick={onRequestClose} 
+                className="react-modal-close"
+            >
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
             <Container>
                 <h2>Cadastrar transação</h2>
                 <input 
